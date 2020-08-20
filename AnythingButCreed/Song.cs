7e1e26a2eace8a1228b71a48e5c95c9876977be4
zcrumpton <z.crumpton@gmail.com>
2020-08-19 18:53:29 -0700

    Add project files.

---
+++ AnythingButCreed/Song.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnythingButCreed
+{
+    class Song
+    {
+        public string song { get; set; }
+        public string artist { get; set; }
+    }
+}
